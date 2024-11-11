d641f5d2b4f21ff22a3446d5c4578109e4e84e39
dev-starlight <50508216+dev-starlight@users.noreply.github.com>
2024-11-11 17:19:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,80 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+import requests
+import pandas as pd
+from datetime import datetime
+from datetime import date, datetime, timedelta
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+# --- SIDEBAR CODE
+def get_allsymbols():
+    url = "https://caddd-index2.vercel.app/allsymbols"
+    headers = {"Content-Type": "application/json"}
+    response = requests.get(url)
+    json_object = response.json()
+    return json_object
+    
+def get_symbol_log(symbol):
+    url = "https://caddd-index2.vercel.app/symbol_log_data?t="+symbol
+    headers = {"Content-Type": "application/json"}
+    response = requests.get(url)
+    json_object = response.json()
+    return json_object
 
-st.header(f'GDP in {to_year}', divider='gray')
+def get_klink(symbol):
+    url = "https://caddd-index2.vercel.app/cklines?symbol="+symbol
+    headers = {"Content-Type": "application/json"}
+    response = requests.get(url, headers=headers)
+    json_object = response.json()
+    Open_time = []
+    Open = []
+    High = []
+    Low = []
+    Close = []
+    for char in json_object["data"]:
+        Open_time.append(char[0])
+        Open.append(char[2])
+        High.append(char[4])
+        Low.append(char[5])
+        Close.append(char[3])
 
-''
+    data = {'Open_time': Open_time,
+        'Open' : Open,
+        'High' : High,
+        'Low' : Low,
+        'Close' : Close}
+    df1 = pd.DataFrame(data)
+    return df1
 
-cols = st.columns(4)
+options = get_allsymbols()
+ticker = st.sidebar.selectbox('Select', options)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+# --- MAIN PAGE
+st.header('MAIN')
+df = get_klink(ticker)
+log = get_symbol_log(ticker)
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+fig = go.Figure(data=[go.Candlestick(x=df['Open_time'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+fig.update_layout(
+                title=ticker + " ticker",
+                yaxis_title= ' Price', width=1200, height=700)
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+for char in log:
+    if float(char['priceChangeM5']) > 2 or float(char['priceChangeM5']) < -2:
+        print(char)
+        fig.add_annotation(x=char['t_now']*1000, y=char['price'],
+            text=char['priceChangeM5'],
+            showarrow=True,
+            arrowhead=7)
+
+st.plotly_chart(fig)
